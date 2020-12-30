056c7d245108c269219d108a8bb2dc5a334ed4ef
Dan Keenan <dk@dankeenan.org>
2020-12-30 17:09:19 -0500

    Migrate from other version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import logo_xeno2 from "./images/xeno2.svg";
+import logo_poketools from "./images/poketools.svg";
+
+const sites = [
+    {name: "Xenoblade Chronicles 2: Management Simulator", image: logo_xeno2, link: "https://xeno2.gamestuff.info/"},
+    {name: "Pokétools", image: logo_poketools, link: "https://poketools.gamestuff.info"},
+];
+
+function Header(props) {
+    return (
+        <header className="mb-auto">
+            <div>
+                <h1 className="display-4">gamestuff.info</h1>
+            </div>
+        </header>
+    );
+}
+
+function Site(props) {
+    const site = props.site;
+    return (
+        <div className="site-link">
+            <a href={site.link}>
+                <img src={site.image} alt={site.name}/>
+            </a>
+        </div>
+    );
+}
+
+function Footer(props) {
+    return (
+        <footer className="mt-auto">
+            <p><a href="https://gitlab.com/gamestuff.info">Contribute to development</a></p>
+        </footer>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const siteLinks = sites.map((site) => <Site key={site.name} site={site}/>);
+
+    return (
+        <div className="d-flex h-100 p-3 mx-auto flex-column">
+            <Header/>
+            <main>
+                <div className="site-links">
+                    {siteLinks}
+                </div>
+            </main>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
