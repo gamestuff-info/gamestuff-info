ef482f92b383f7f6ed71e4cf451496957f53ab22
Dan Keenan <dk@dankeenan.org>
2021-01-10 10:11:06 -0500

    Use react-snapshot

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-snapshot'
 import 'jquery';
 import 'popper.js';
 import 'bootstrap';
 import './App.scss';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
